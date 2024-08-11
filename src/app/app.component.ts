6bfa9d055fc514f81de337715fd699e617a35a08
Marco Ammann <marco-ammann@outlook.com>
2024-08-11 15:40:38 +0200

    changed some stuff up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminInitializationService } from './services/admin-initialization.service';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { HeaderComponent } from './header/header.component';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDivider } from '@angular/material/divider';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AuthService } from './services/auth.service';
+import { LoginDialogComponent } from './components/login-dialog/login-dialog.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -16,21 +20,28 @@ import { MatDivider } from '@angular/material/divider';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   imports: [
+    CommonModule,
     RouterModule,
     AdminDashboardComponent,
     MatSidenavModule,
     MatButtonModule,
     MatIconModule,
     MatToolbarModule,
-    HeaderComponent,
     MatChipsModule,
-    MatDivider
+    MatDivider,
+    RouterLink,
+    RouterLinkActive
   ],
 })
 export class AppComponent implements OnInit {
   title = 'Choscht.';
-
-  constructor(private adminInitializationService: AdminInitializationService) {}
+  isLoggedIn: boolean = false;
+  constructor(
+    private adminInitializationService: AdminInitializationService,
+    private authService: AuthService,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.adminInitializationService
@@ -41,5 +52,21 @@ export class AppComponent implements OnInit {
       .catch((error) => {
         console.error('Error initializing admin account:', error);
       });
+
+    this.authService.isAuthenticated().subscribe((authenticated) => {
+      this.isLoggedIn = authenticated;
+      console.log('is logged in = ', this.isLoggedIn);
+    });
+  }
+
+  openLoginDialog() {
+    this.dialog.open(LoginDialogComponent);
+  }
+
+  logout(): void {
+    this.authService.logout().then(() => {
+      this.isLoggedIn = false;
+      this.snackBar.open('Sie wurden abgemeldet.', 'Close', { duration: 3000 });
+    });
   }
 }
